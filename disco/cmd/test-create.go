37af837398b28728f537f996b74d1af635d249b7
Francisco <bonand04@gmail.com>
2023-07-08 13:57:06 -0300

    feat: working version of 'disco test create'

--- disco/cmd/test-create.go
+++ disco/cmd/test-create.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"path/filepath"
 
 	"github.com/hfscheid/ai-project/disco/pkg/cmd"
+	"github.com/hfscheid/ai-project/disco/pkg/config"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +14,24 @@ import (
 func (d *Disco) newTestCreateCmd() *cobra.Command {
     return cmd.NewCmd("create").
         WithDescription("Command for creating tests").
-        WithExample("Creates a test", "test create").
-        NoArgs(func(ctx context.Context, c *cobra.Command) error { return nil })
+        WithExample("Creates a test", "test create /path/to/test.yaml").
+        ExactArgs(1, d.createTest)
+}
+
+func (d *Disco) createTest(ctx context.Context, args []string) error {
+    currDir, err := filepath.Abs(".")
+    if err != nil {
+        return fmt.Errorf("Error getting absolute path to curr dir: %v", err)
+    }
+    cfgFile := filepath.Join(currDir, args[0])
+    testCase, err := config.ReadTestConfig(cfgFile)
+    if err != nil {
+        return err
+    }
+    if _, ok := d.tests.TestCases[testCase.Name]; !ok {
+        d.tests.TestCases[testCase.Name] = testCase
+        return nil
+    } else {
+        return fmt.Errorf("Test with the same name already exists, must be unique")
+    }
 }
