40f609e9941dfed8059439da7636a2242809f1c5
Henrique Furst Scheid <hfscheid@gmail.com>
2023-07-08 17:16:50 -0300

    feat: test-run implemented

--- disco/cmd/test-run.go
+++ disco/cmd/test-run.go
@@ -2,8 +2,13 @@ package cmd
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"sync"
 
 	"github.com/hfscheid/ai-project/disco/pkg/cmd"
+	"github.com/hfscheid/ai-project/disco/pkg/config"
+	"github.com/hfscheid/ai-project/disco/pkg/docker"
 	"github.com/spf13/cobra"
 )
 
@@ -12,5 +17,80 @@ func (d *Disco) newTestRunCmd() *cobra.Command {
     return cmd.NewCmd("run").
         WithDescription("Command for running selected test").
         WithExample("Run selected test", "test run").
-        NoArgs(func(ctx context.Context, c *cobra.Command) error { return nil })
+        NoArgs(d.runTest)
+}
+
+func (d *Disco) runTest(ctx context.Context, c *cobra.Command) error {
+    test := d.selectedTest
+    var wg sync.WaitGroup
+
+    nwInfo := dockerTranslateNw(test.Network)
+    if nwId, err := d.dockerC.CreateNetwork(ctx, nwInfo);
+    err != nil {
+        return fmt.Errorf("Network %s: %q", nwId, err)
+    }
+
+    cntInfos := dockerTranslateContainers(test.Containers, nwInfo.NetworkName)
+    wg.Add(len(cntInfos))
+    errCh := make(chan error)
+    errA := []error{}
+    go func(errCh chan error, errA []error) {
+        for {
+            err, ok := <- errCh
+            if ok {
+                errA = append(errA, err)
+            } else {
+                return
+            }
+        }
+    }(errCh, errA)
+    for _, c := range cntInfos {
+        go func(ctx context.Context, c *docker.ContainerInfo, wg *sync.WaitGroup, errCh chan error) {
+            id, err := d.dockerC.RunContainer(ctx, *c)
+            if err != nil {
+                errCh <- fmt.Errorf("%v: %q", id, err)
+            }
+            wg.Done()
+        }(ctx, &c, &wg, errCh)
+    }
+    wg.Wait()
+    close(errCh)
+    return errors.Join(errA...)
+}
+
+func dockerTranslateNw(nw *config.Network) docker.NetworkInfo {
+    return docker.NetworkInfo{
+        NetworkName: nw.Name,
+        Subnet: nw.Subnet,
+        Gateway: nw.Gateway,
+    }
+}
+
+func dockerTranslateContainers(cs []*config.Container, nwName string) []docker.ContainerInfo {
+    cInfos := make([]docker.ContainerInfo, len(cs))
+    for i, c := range(cs) {
+        baseImage, imageVersion, volumeTarget := 
+            func () (string, string, string) {
+            switch c.Type {
+            case config.EXABGP:
+                return "something", "latest", "/etc/exabgp"
+            case config.BIRD:
+                return "pierky/bird", "latest", "/etc/bird"
+            case config.FRR:
+                return "quay.io/frrouting/frr", "8.5.1", "/etc/frr"
+            default:
+                return "", "", ""
+            }
+        }()
+        cInfo := docker.ContainerInfo {
+            ContainerName: c.Name,
+            NetworkName: nwName,
+            BaseImage: baseImage,
+            ImageVersion: imageVersion,
+            VolumeTarget: volumeTarget,
+            VolumeSource: c.ConfigPath,
+        }
+        cInfos[i] = cInfo
+    }
+    return cInfos
 }
