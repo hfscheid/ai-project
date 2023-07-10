925d64c193ce4bcd10c8d11313862fcc0e0e50b3
Francisco <bonand04@gmail.com>
2023-07-09 21:26:33 -0300

    feat: command 'disco test select'

--- disco/cmd/test-select.go
+++ disco/cmd/test-select.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hfscheid/ai-project/disco/pkg/cmd"
 	"github.com/spf13/cobra"
@@ -11,6 +12,15 @@ import (
 func (d *Disco) newTestSelectCmd() *cobra.Command {
     return cmd.NewCmd("select").
         WithDescription("Command for selecting a test").
-        WithExample("Select a test", "test select").
-        NoArgs(func(ctx context.Context, c *cobra.Command) error { return nil })
+        WithExample("Select a test", "test select <test name>").
+        ExactArgs(1, d.selectTest)
+}
+
+func (d *Disco) selectTest(ctx context.Context, s []string) error {
+    testName := s[0]
+    if _, ok := d.tests.TestCases[testName]; !ok {
+        return fmt.Errorf("Unable to find test %q", testName)
+    }
+    d.selectedTest = d.tests.TestCases[testName]
+    return nil
 }
